7d422f050a9d9812ab34b71d64675024649f6439
Brian Radebaugh <rivukis@gmail.com>
2018-03-03 23:04:42 -0800

    fixed forcible issue, renamed forceable to forcible

--- Forcible.swift
+++ Forcible.swift
@@ -1,14 +1,14 @@
 
-protocol Forceable {
+protocol Forcible {
     associatedtype ForceCastJustification
     static func cast<T, U>(object instance: T, to _: U.Type, justification: ForceCastJustification) -> U
     static func cast<T, U>(object instance: T?, to _: U.Type, justification: ForceCastJustification) -> U
 
     associatedtype ForceUnwrapJustification
-    static func unwrap<T>(object instance: T?, justification: ForceCastJustification) -> T
+    static func unwrap<T>(object instance: T?, justification: ForceUnwrapJustification) -> T
 }
 
-extension Forceable {
+extension Forcible {
     static func cast<T, U>(object: T, to _: U.Type, justification: ForceCastJustification) -> U {
         guard let castedObject = object as? U else {
             fatalError("Force casting \(String(describing: object)) to \(U.self) did not work. Justification given: \(justification)")
@@ -25,7 +25,7 @@ extension Forceable {
         return castedObject
     }
 
-    static func unwrap<T>(object: T?, justification: ForceCastJustification) -> T {
+    static func unwrap<T>(object: T?, justification: ForceUnwrapJustification) -> T {
         guard let castedObject = object else {
             fatalError("Force unwrapping \(String(describing: object)) did not work. Justification given: \(justification)")
         }
