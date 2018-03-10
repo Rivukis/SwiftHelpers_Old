2bca4c2c213c0dd96f53b3f7ccb46982d27aa0e3
Jake Bromberg <jake.bromberg@gmail.com>
2018-03-10 13:24:22 -0800

    Update LinkedListNode.swift
    Using conditional conformance to broaden the applicability of the class. Abstracting out the deep copy stuff into its own class. Conforming `LLNode` to `Sequence`.

--- LinkedListNode.swift
+++ LinkedListNode.swift
@@ -1,38 +1,56 @@
 
-public class LLNode<T: Equatable & Comparable & CustomStringConvertible> {
+public final class LLNode<T> {
     public var next: LLNode<T>?
     public var value: T
-
-    public init(value: T, next: LLNode<T>? = nil) {
+    
+    public required init(value: T, next: LLNode<T>? = nil) {
         self.value = value
         self.next = next
     }
+}
 
-    public func deepCopy() -> LLNode {
-        return LLNode(value: value, next: next?.deepCopy())
+public protocol Copiable: class {
+    func copy() -> Self
+}
+
+extension LLNode: Copiable where T: Copiable {
+    public func copy() -> LLNode {
+        return LLNode(value: value.copy(), next: next?.copy())
     }
 }
 
-extension LLNode: Equatable {
+extension LLNode: Equatable where T: Equatable {
     public static func ==(lhs: LLNode<T>, rhs: LLNode<T>) -> Bool {
         guard lhs.value == rhs.value else {
             return false
         }
-
+        
         return lhs.next == rhs.next
     }
 }
 
-extension LLNode: Comparable {
-    public static func <(lhs: LLNode<T>, rhs: LLNode<T>) -> Bool {
-        return lhs.value < rhs.value
+extension LLNode: CustomStringConvertible where T: CustomStringConvertible {
+    public var description : String {
+        let nextDescription = next != nil ? " --> " + String(describing: next!) : ""
+        
+        return String(describing: value) + nextDescription
     }
 }
 
-extension LLNode: CustomStringConvertible {
-    public var description : String {
-        let nextDescription = next != nil ? " --> " + String(describing: next!) : ""
-
-        return String(describing: value) + nextDescription
+extension LLNode: Sequence {
+    public final class LLIterator<T>: IteratorProtocol {
+        var head: LLNode
+        
+        init(head: LLNode) {
+            self.head = head
+        }
+        
+        public func next() -> T? {
+            return nil
+        }
+    }
+    
+    public func makeIterator() -> LLIterator<T> {
+        return LLIterator(head: self)
     }
 }
